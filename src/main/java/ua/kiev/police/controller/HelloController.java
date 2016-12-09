0b1a5dce0efbd8fdcd451a26ace2fd243bb685ba
accepts <accepts@ya.ru>
2016-12-09 10:37:45 +0200

    Day #5 - Spring Security added. Start to crating CAR entity

--- src/main/java/ua/kiev/police/controller/HelloController.java
+++ src/main/java/ua/kiev/police/controller/HelloController.java
@@ -3,32 +3,18 @@ package ua.kiev.police.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartFile;
 import ua.kiev.police.dao.PersonDao;
 import ua.kiev.police.model.Person;
-import ua.kiev.police.model.enums.Rank;
 import ua.kiev.police.util.LoggerWrapper;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 @Controller
 public class HelloController {
 
 	protected static final LoggerWrapper LOG = LoggerWrapper.get(HelloController.class);
-
-	private Path path;
 
 	@Autowired
 	private PersonDao personDao;
@@ -54,129 +40,7 @@ public class HelloController {
 		return "personView";
 	}
 
-	@RequestMapping("/admin")
-	public String adminPage(){
-		return "admin";
-	}
-
-
-	@RequestMapping("/admin/personInventory")
-	public String personInventory(Model model){
-		List<Person> personList = personDao.getAllPersons();
-		model.addAttribute("personList", personList);
-		return "personInventory";
-	}
-
-
-	@RequestMapping("/admin/personInventory/addPerson")
-	public String addPerson(Model model){
-		Person person = new Person();
-		person.setRank(Rank.OFFICER);
-		model.addAttribute("person", person);
-		return "personAdd";
-	}
-
-	@RequestMapping(value = "/admin/personInventory/addPerson", method = RequestMethod.POST)
-	public String addPersonPost(@Valid @ModelAttribute("person") Person person,
-								BindingResult result, HttpServletRequest request){
-
-		if (result.hasErrors()){
-			return "personAdd";
-		}
-
-		personDao.addPerson(person);
-
-		MultipartFile personImage = person.getPersonImage();
-		String rootDirectory = request.getSession().getServletContext().getRealPath("/");
-		path = Paths.get(rootDirectory + "\\WEB-INF\\resources\\photo_person\\" + person.getPersonId() + ".png");
-		if (personImage != null && !personImage.isEmpty()) {
-			try {
-				personImage.transferTo(new File(path.toString()));
-			} catch (IOException e) {
-				e.printStackTrace();
-				LOG.info("Error occur when try to save file to path {}" + path.toString());
-				throw new RuntimeException("Person image saving failed!", e);
-			}
-		}
-
-		return "redirect:/admin/personInventory/";
-	}
-
-
-	@RequestMapping("/admin/personInventory/deletePerson/{personId}")
-	public String deletePerson(@PathVariable int personId, Model model, HttpServletRequest request){
-
-		String rootDirectory = request.getSession().getServletContext().getRealPath("/");
-		path = Paths.get(rootDirectory + "\\WEB-INF\\resources\\photo_person\\" + personId + ".png");
-
-		if (Files.exists(path)){
-			try {
-				Files.delete(path);
-			} catch (IOException e) {
-				LOG.info("Error occur when try to delete file with path {}" + path.toString());
-				e.printStackTrace();
-			}
-		}
-
-		personDao.deletePerson(personId);
-		return "redirect:/admin/personInventory";
-	}
-
-
-
-	@RequestMapping("/admin/personInventory/editPerson/{personId}")
-	public String editPerson(@PathVariable("personId") int personId, Model model){
-		Person person = personDao.getPersonById(personId);
-		model.addAttribute(person);
-		return "personEdit";
-	}
-
-	@RequestMapping(value = "/admin/personInventory/editPerson/", method = RequestMethod.POST)
-	public String editPerson(@Valid @ModelAttribute("person") Person person, BindingResult result, Model model, HttpServletRequest request){
-		if (result.hasErrors()){
-			return "personEdit";
-		}
-
-		MultipartFile personImage = person.getPersonImage();
-		String rootDirectory = request.getSession().getServletContext().getRealPath("/");
-		path = Paths.get(rootDirectory + "\\WEB-INF\\resources\\photo_person\\" + person.getPersonId() + ".png");
-
-		if (personImage != null && !personImage.isEmpty()) {
-			try {
-				personImage.transferTo(new File(path.toString()));
-			} catch (IOException e) {
-				e.printStackTrace();
-				LOG.info("Error occur when edit person object");
-				throw new RuntimeException("Product image saving failed!", e);
-			}
-		}
-		personDao.editPerson(person);
-		return "redirect:/admin/personInventory/";
-	}
-
 
 
 }
 
-/*
-
-
-@RequestMapping(value = "/admin/productInventory/editProduct", method=RequestMethod.POST)
-public String editProduct(@ModelAttribute("product") Product product, Model model, HttpServletRequest request){
-    MultipartFile productImage = product.getProductImage();
-    String rootDirectory = request.getSession().getServletContext().getRealPath("/");
-    path = Paths.get(rootDirectory + "\\WEB-INF\\resources\\images\\" + product.getProductId() + ".png");
-
-    if (productImage != null && !productImage.isEmpty()) {
-        try {
-            productImage.transferTo(new File(path.toString()));
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Product image saving failed!", e);
-        }
-    }
-    productDao.editProduct(product);
-    return "redirect:/admin/productInventory";
-}
-
- */
--- src/main/webapp/WEB-INF/pages/admin.jsp
+++ src/main/webapp/WEB-INF/pages/admin.jsp
@@ -5,13 +5,25 @@
 <div class="page-header">
   <h1>Admin page</h1>
   <p>Add some product</p>
+
+  <c:if test="${pageContext.request.userPrincipal.name != null }">
+    <h2>
+      Welcome: ${pageContext.request.userPrincipal.name} <br/>
+      <a href="<c:url value="/j_spring_security_logout"/>  " class="btn btn-danger" > Logout</a>
+    </h2>
+  </c:if>
+
+
 </div>
+
+
 
 <div class="container col-md-10">
 
   <h3>
     <a href="<c:url value="/admin/personInventory"/> " >Person inventory </a>
   </h3>
+
 
 
 
